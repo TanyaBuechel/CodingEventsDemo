468c26e9dc0cf5deb142ebc20912f77e4f623718
Tanya Buechel <tanyabuechel@gmail.com>
2022-11-09 21:19:15 -0600

    Finished exercise

---
+++ CodingEventsDemo/Controllers/EventCategoryController.cs
@@ -0,0 +1,29 @@
+﻿using CodingEventsDemo.Data;
+using CodingEventsDemo.Models;
+using CodingEventsDemo.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodingEventsDemo.Controllers
+{
+    public class EventCategoryController : Controller
+    {
+        private EventDbContext context;
+
+        public EventCategoryController(EventDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.title = "All Categories";
+            List<EventCategory> categories = context.Categories.AsNoTracking().ToList();
+            return View(categories);
+        }
+    }
+}
