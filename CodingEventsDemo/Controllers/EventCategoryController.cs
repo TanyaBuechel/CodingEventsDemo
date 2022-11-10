2b2b29d4c4e030ae0a621471d27f8b6888fe5255
Tanya Buechel <tanyabuechel@gmail.com>
2022-11-09 21:07:24 -0600

    Finished studio

--- CodingEventsDemo/Controllers/EventCategoryController.cs
+++ CodingEventsDemo/Controllers/EventCategoryController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using CodingEventsDemo.Data;
 using CodingEventsDemo.Models;
+using CodingEventsDemo.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -26,6 +27,36 @@ namespace CodingEventsDemo.Controllers
             ViewBag.title = "All Categories";
             List<EventCategory> categories = context.Categories.ToList();
             return View(categories);
+        }
+
+        // GET: /<controller>/
+        [HttpGet]
+        [Route("/Create")]
+        public IActionResult Create()
+        {
+            AddEventCategoryViewModel addEventCategoryViewModel = new AddEventCategoryViewModel();
+
+            return View(addEventCategoryViewModel);
+        }
+
+        [HttpPost]
+        [Route("/EventCategory/Index")]
+        public IActionResult ProcessCreateEventCategoryForm(AddEventCategoryViewModel addEventCategoryViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                EventCategory newCategory = new EventCategory
+                {
+                    Name = addEventCategoryViewModel.Name,
+                };
+
+                context.Categories.Add(newCategory);
+                context.SaveChanges();
+
+                return Redirect("/Index");
+            }
+
+            return View("/EventCategory/Create", addEventCategoryViewModel);
         }
     }
 }
